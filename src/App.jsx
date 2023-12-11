92b613ea4d483ea0f9eb37b1909652f85f9b928b
AkashRiaz <mdriaz191051@gmail.com>
2023-12-11 23:11:12 +0600

    initial setup is done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+    <h1 className='text-4xl font-bold'>Md Akashuzzaman Riaz</h1>  
+    <h2>Md Akashuzzaman Riaz</h2>  
+    <h3>Md Akashuzzaman Riaz</h3>  
+    </div>
+  );
+};
+
+export default App;
